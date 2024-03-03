a007680c97e56a8cc2cb4c58cbc2142982d85c60
Rafael Jardim <rafaelmjardim@gmail.com>
2024-03-03 01:15:53 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LucideAngularModule, Home, PackageSearch, ShoppingCart } from 'lucide-angular';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
