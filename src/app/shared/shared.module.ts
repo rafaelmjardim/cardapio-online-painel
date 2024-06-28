e87406596ef87f4893556b152995fd01a5644361
Rafael Jardim <rafaelmjardim@gmail.com>
2024-06-28 20:18:01 -0300

    concluido dashboard

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import {MatDialogModule} from '@angular/material/dialog';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from '../components/header/header.component';
 
-import { LucideAngularModule, Home, PackageSearch, ShoppingCart, X, User, LogOut, Search, Archive, Box } from 'lucide-angular';
+import { LucideAngularModule, Home, PackageSearch, ShoppingCart, X, User, LogOut, Search, Archive, Box, BadgeDollarSign, HandCoins } from 'lucide-angular';
 
 
 
@@ -17,7 +17,7 @@ import { LucideAngularModule, Home, PackageSearch, ShoppingCart, X, User, LogOut
   imports: [
     CommonModule,
     MatDialogModule,
-    LucideAngularModule.pick({Home, PackageSearch, ShoppingCart, X, User, LogOut, Search, Archive, Box}),
+    LucideAngularModule.pick({Home, PackageSearch, ShoppingCart, X, User, LogOut, Search, Archive, Box, BadgeDollarSign, HandCoins}),
   ],
   exports: [
     MatDialogModule,
